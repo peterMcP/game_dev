662cbfb2370311a8d1fd51bd10032d01ba8d3ae7
peterMcP <36275626+peterMcP@users.noreply.github.com>
2018-09-26 11:56:53 +0200

    added class4 handout (to do)

---
+++ Dev_class4_handout/Motor2D/j1Map.h
@@ -0,0 +1,90 @@
+#ifndef __j1MAP_H__
+#define __j1MAP_H__
+
+#include "PugiXml/src/pugixml.hpp"
+#include "p2List.h"
+#include "p2Point.h"
+#include "j1Module.h"
+
+// TODO 2: Create a struct to hold information for a TileSet
+// Ignore Terrain Types and Tile Types for now, but we want the image!
+// ----------------------------------------------------
+struct TileSetInfo
+{
+	uint firstgid = 0;
+	char* name = "defaultName";
+	uint tilewidth = 0;
+	uint tileheight = 0;
+	int spacing = -1;
+	int margin = -1;
+};
+
+
+// TODO 1: Create a struct needed to hold the information to Map node
+
+enum class renderOrder
+{
+	error = -1,
+	right_down,
+	right_up,
+	down_right,
+	down_left
+};
+
+enum class orientation
+{
+	orthogonal,
+	isometric
+};
+
+struct dataMap
+{
+	float version = 0;
+	renderOrder renderOrder = renderOrder::error;
+	orientation orientation = orientation::orthogonal;
+	uint width = 0;
+	uint height = 0; 
+	uint tilewidth = 0;
+	uint tileheight = 0;
+};
+
+
+// ----------------------------------------------------
+class j1Map : public j1Module
+{
+public:
+
+	j1Map();
+
+	// Destructor
+	virtual ~j1Map();
+
+	// Called before render is available
+	bool Awake(pugi::xml_node& conf);
+
+	// Called each loop iteration
+	void Draw();
+
+	// Called before quitting
+	bool CleanUp();
+
+	// Load new map
+	bool Load(const char* path);
+
+private:
+
+	bool loadMapData(pugi::xml_node&);
+
+public:
+
+	// TODO 1: Add your struct for map info as public for now
+	dataMap mapInfo;
+
+private:
+
+	pugi::xml_document	map_file;
+	p2SString			folder;
+	bool				map_loaded;
+};
+
+#endif // __j1MAP_H__
