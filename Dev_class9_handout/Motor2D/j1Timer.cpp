ef312a3e89282c400df37676970c8e6d43158739
peterMcP <36275626+peterMcP@users.noreply.github.com>
2018-11-07 11:57:06 +0100

    some todos done

--- Dev_class9_handout/Motor2D/j1Timer.cpp
+++ Dev_class9_handout/Motor2D/j1Timer.cpp
@@ -17,16 +17,17 @@ void j1Timer::Start()
 {
 	// TODO 1: Fill Start(), Read(), ReadSec() methods
 	// they are simple, one line each!
+	started_at = SDL_GetTicks();
 }
 
 // ---------------------------------------------
 uint32 j1Timer::Read() const
 {
-	return 0;
+	return SDL_GetTicks();
 }
 
 // ---------------------------------------------
 float j1Timer::ReadSec() const
 {
-	return 0.0f;
+	return SDL_GetTicks() * 0.001f;
 }
